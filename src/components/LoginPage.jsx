39c29fcb8930025d4a77270a131a3d838a5ddd00
Ragul D <raguld2105@gmail.com>
2025-02-24 01:03:31 +0530

    login page route

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaLock, FaEnvelope } from 'react-icons/fa';
 import { HiChip, HiCube } from 'react-icons/hi';
